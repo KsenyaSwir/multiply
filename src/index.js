6d3def77b5c0875e74bb0c6381d8b372f7eb2b94
Kseniya Sviridova <ksyu.sviridova.eger@mail.ru>
2019-10-19 22:55:53 +0300

    feat:add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function multiply(first, second) {
-  // your solution
+module.exports = function multiply(a, b) {
+    a = a.split('').reverse();
+    b = b.split('').reverse();
+    var result = [];
+
+    for (var i = 0; a[i] >= 0; i++) {
+        for (var j = 0; b[j] >= 0; j++) {
+            if (!result[i + j]) {
+                result[i + j] = 0;
+            }
+
+            result[i + j] += a[i] * b[j];
+        }
+    }
+
+    for (i = 0; result[i] >= 0; i++) {
+        if (result[i] >= 10) {
+            if (!result[i + 1]) {
+                result[i + 1] = 0;
+            }
+
+            result[i + 1] += parseInt(result[i] / 10);
+            result[i] %= 10;
+        }
+    }
+
+    result.reverse();
+    for (i = 0; i < result.length; i++) {
+        if (result[i] === 0) {
+            result.splice(i--, 1);
+        } else {
+            i = result.length * 2;
+        }
+    }
+
+    return result.join('');
 }
